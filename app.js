4be81d6fb635dde555956008eaa0ff28d68fda27
huy hoang <hoangtronghuy91@gmail.com>
2021-11-21 23:27:19 +0700

    first commit

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 
 //Cấu hình nhận APi
 app.use((req, res, next)=>{
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:57880');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     res.setHeader('Access-Control-Allow-Credentials', true);
